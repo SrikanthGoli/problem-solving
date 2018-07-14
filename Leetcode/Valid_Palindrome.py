a55a2da461f39eac67461dfb8c9f90b119ed8f0c
srikanthgoli <srikanthgoli018@gmail.com>
2018-07-14 02:00:20 -0400

    #125 Valid Palindrome

---
+++ Leetcode/Valid_Palindrome.py
@@ -0,0 +1,21 @@
+
+#125. Valid Palindrome
+
+def isPalindrome(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    input_lower = s.lower()
+    clean_input = re.sub("[^a-z0-9]+", "", input_lower)
+
+    i = 0
+    j = len(clean_input)-1
+    while i < j:
+        if clean_input[i] != clean_input[j]:
+            return False
+        else:
+            i += 1
+            j -= 1
+
+    return True
