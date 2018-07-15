618bd260897ae939f17b310c3e39ddb2544032ea
srikanthgoli <srikanthgoli018@gmail.com>
2018-07-15 16:54:07 -0400

    #557. Reverse Words in a String III

---
+++ Leetcode/reverseWords.py
@@ -0,0 +1,22 @@
+
+#557. Reverse Words in a String III
+
+def reverseWords(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    output = ""
+    for i in s.split():
+        if len(output) == 0:
+            output += i[::-1]
+        else:
+            output += " "+i[::-1]
+
+    return output
+
+
+input = "Let's take LeetCode contest"
+output = ""
+
+print(reverseWords(input))
