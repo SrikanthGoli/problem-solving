8dabf581a3b8a4e748501a4c443bd766da3eeb24
srikanthgoli <srikanthgoli018@gmail.com>
2018-07-24 10:09:50 -0400

    # 67. Add Binary - Need to think of more efficient solution

--- Leetcode/addBinary.py
+++ Leetcode/addBinary.py
@@ -8,30 +8,50 @@ def addBinary(a, b):
     """
 
     i, j = len(a)-1, len(b)-1
-    count = 0
+    carry = 0
     res = ""
 
     while i >= 0 and j >= 0:
-        if a[i] + b[j] + count == 2:
-            count = 1
+        if int(a[i]) + int(b[j]) + carry == 2:
+            carry = 1
             res += '0'
-            i -= 1
-            j -= 1
-        elif a[i] + b[j] + count == 3:
-            count = 1
+        elif int(a[i]) + int(b[j]) + carry == 3:
+            carry = 1
             res += '1'
-            i -= 1
-            j -= 1
-        elif a[i] + b[j] + count == 1:
-            count = 0
+        elif int(a[i]) + int(b[j]) + carry == 1:
+            carry = 0
             res += '1'
-            i -= 1
-            j -= 1
         else:
-            count = 0
+            carry = 0
             res += '0'
-            j -= 1
-            i -= 1
+        i -= 1
+        j -= 1
 
     while i >= 0:
+        if int(a[i]) + carry == 2:
+            carry = 1
+            res += '0'
+        elif int(a[i]) + carry == 1:
+            carry = 0
+            res += '1'
+        else:
+            carry = 0
+            res += '0'
+        i -= 1
 
+    while j >= 0:
+        if int(b[j]) + carry == 2:
+            carry = 1
+            res += '0'
+        elif int(b[j]) + carry == 1:
+            carry = 0
+            res += '1'
+        else:
+            carry = 0
+            res += '0'
+        j -= 1
+
+    if carry > 0:
+        res += str(carry)
+
+    return res[::-1]
