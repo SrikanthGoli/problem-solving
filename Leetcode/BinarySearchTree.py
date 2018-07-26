ad5ffe0b92a2e4a3a4a385684ac589de31899a78
srikanthgoli <srikanthgoli018@gmail.com>
2018-07-26 19:43:28 -0400

    Binary search tree

---
+++ Leetcode/BinarySearchTree.py
@@ -0,0 +1,15 @@
+
+# Binary Search Tree
+
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.parent = None
+        self.left = None
+        self.right = None
+
+
+class binarySearchTree(object):
+
+
