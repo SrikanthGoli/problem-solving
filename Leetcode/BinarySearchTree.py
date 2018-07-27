35090e70fe85261a789d6a4619e3f5f4bb18ad64
srikanthgoli <srikanthgoli018@gmail.com>
2018-07-26 20:26:40 -0400

    Binary Search Tree with simple push function

--- Leetcode/BinarySearchTree.py
+++ Leetcode/BinarySearchTree.py
@@ -3,7 +3,7 @@
 
 class Node(object):
 
-    def __init__(self, data):
+    def __init__(self, data = None):
         self.data = data
         self.parent = None
         self.left = None
@@ -12,4 +12,37 @@ class Node(object):
 
 class binarySearchTree(object):
 
+    def __init__(self):
+        self.root = Node()
 
+    def push(self, data):
+
+        new_node = Node(data)
+        curr_node = self.root
+
+        if curr_node.data == None:
+            curr_node.data = data
+
+        else:
+            while curr_node.left != None and curr_node.right != None:
+                if curr_node.data < new_node.data:
+                    curr_node = curr_node.left
+                else:
+                    curr_node = curr_node.right
+
+            if curr_node.data < data:
+                curr_node.left = new_node
+                curr_node.left.parent = curr_node
+            else:
+                curr_node.right = new_node
+                curr_node.right.parent = curr_node
+
+
+    def display(self):
+
+
+
+tree = binarySearchTree()
+tree.push(50)
+tree.push(40)
+tree.push(60)
