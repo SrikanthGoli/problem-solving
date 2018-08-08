14ccb8a884fa6aa03906f6076beb2286ba406534
srikanthgoli <srikanthgoli018@gmail.com>
2018-08-07 23:17:34 -0400

    binary search

--- Leetcode/binarySearch.py
+++ Leetcode/binarySearch.py
@@ -3,20 +3,15 @@
 
 def binarySearch(key, input, left, right):
 
-    if left > right:
-        return False
+    if right >= left:
 
-    mid = (left+right)//2
+        mid = (left+right)//2
 
-    if key == input[mid]:
-        return mid
-    elif key < input[mid]:
-        binarySearch(key, input, left, mid)
-    else:
-        binarySearch(key, input, mid+1, right)
+        if input[mid] == key:
+            return mid
+        elif key < input[mid]:
+            return binarySearch(key, input, left, mid)
+        else:
+            return binarySearch(key, input, mid+1, right)
 
-    return None
-
-
-test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14]
-print(binarySearch(9, test, 0, 11))
+    return False
