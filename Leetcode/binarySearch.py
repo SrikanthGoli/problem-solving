fec192c070d4809d2e83acaa06c1e5ca486bedae
srikanthgoli <srikanthgoli018@gmail.com>
2018-08-08 11:57:11 -0400

    Binary Search - Iterative and Reccursive versions

--- Leetcode/binarySearch.py
+++ Leetcode/binarySearch.py
@@ -1,7 +1,6 @@
 
 # Binary Search - (O)logn
-
-def binarySearch(key, input, left, right):
+def binarySearch_recursive(key, input, left, right):
 
     if right >= left:
 
@@ -10,8 +9,27 @@ def binarySearch(key, input, left, right):
         if input[mid] == key:
             return mid
         elif key < input[mid]:
-            return binarySearch(key, input, left, mid)
+            return binarySearch_recursive(key, input, left, mid)
         else:
-            return binarySearch(key, input, mid+1, right)
+            return binarySearch_recursive(key, input, mid+1, right)
+
+    return False
+
+
+def binarySearch_iterative(key, input):
+
+    left = 0
+    right = len(input)-1
+
+    while right >= left:
+
+        mid = (left + right)//2
+
+        if key == input[mid]:
+            return mid
+        elif key < input[mid]:
+            right = mid-1
+        elif key > input[mid]:
+            left = mid+1
 
     return False
